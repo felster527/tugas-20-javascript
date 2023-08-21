9d282178d18d60e35d9e6ec3a2dee463b59842a0
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 19:35:12 +0700

    Add files via upload

---
+++ Tugas20.js
@@ -0,0 +1,10 @@
+var cari = (x)=> {
+    x = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+    console.log(x.match(/are/))
+    console.log()
+    console.log(x.match(/words/))
+
+}
+
+cari()
